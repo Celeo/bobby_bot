b126e3ad1e147f8f05e9c921501713e6ae2351d7
Celeo <mattboulanger@fastmail.com>
2023-05-11 12:59:37 -0700

    Rewrite it in Rust

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ A joke Discord bot.
 
 ## Installing
 
-1. Install [Deno](https://deno.land)
+1. Install [Rust](https://www.rust-lang.org/)
 1. Clone the repo
 
 ## Using
 
 1. Create an `.env` file with your Discord bot token, like `DISCORD_BOT_TOKEN=your_token_here`
-1. Run with `deno task run`
+1. Run with `cargo r`
 
 ## License
 
@@ -24,3 +24,4 @@ Note: preceding my normal "contributions welcome" message is a disclaimer that t
 Please feel free to contribute. Please open an issue first (or comment on an existing one) so that I know that you want to add/change something.
 
 Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you shall be licensed as above, without any additional terms or conditions.
+
---
+++ src/main.rs
@@ -0,0 +1,126 @@
+#![deny(clippy::all, clippy::pedantic)]
+
+use anyhow::Result;
+use base64::{engine::general_purpose, Engine as _};
+use dotenv::dotenv;
+use log::{error, info, warn};
+use rand::{seq::SliceRandom, Rng};
+use std::{
+    collections::HashMap,
+    env,
+    sync::{Arc, Mutex},
+};
+use twilight_gateway::{Event, Intents, Shard, ShardId};
+use twilight_http::Client as HttpClient;
+use twilight_model::id::Id;
+
+mod messages;
+use messages::MESSAGES;
+
+const MESSAGE_RESPONSE_THRESHOLD: u64 = 1_000;
+
+/// Parse a bot ID from the token.
+///
+/// This function panics instead of returning a Result, as the token
+/// must confirm to this layout in order to be valid for Discord.
+fn bot_id_from_token(token: &str) -> u64 {
+    std::str::from_utf8(
+        &general_purpose::STANDARD_NO_PAD
+            .decode(token.split('.').next().unwrap())
+            .unwrap(),
+    )
+    .unwrap()
+    .parse()
+    .unwrap()
+}
+
+/// Entrypoint.
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+    pretty_env_logger::init();
+
+    let token = env::var("DISCORD_BOT_TOKEN").expect("Missing 'DISCORD_BOT_TOKEN' env var");
+    let bot_id = bot_id_from_token(&token);
+    let intents = Intents::GUILD_MESSAGES | Intents::MESSAGE_CONTENT;
+    let mut shard = Shard::new(ShardId::ONE, token.clone(), intents);
+    let http = Arc::new(HttpClient::new(token));
+    let message_count: Arc<Mutex<HashMap<u64, u64>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    info!("Waiting for events");
+    loop {
+        let event = match shard.next_event().await {
+            Ok(event) => event,
+            Err(source) => {
+                warn!("Error receiving event: {:?}", source);
+                if source.is_fatal() {
+                    break;
+                }
+                continue;
+            }
+        };
+        let last_seen = Arc::clone(&message_count);
+        let http = Arc::clone(&http);
+        tokio::spawn(async move {
+            if let Err(e) = handle_event(event, http, bot_id, last_seen).await {
+                error!("Error in future: {e}");
+            }
+        });
+    }
+}
+
+/// Handle a single Event from the Discord Gateway.
+async fn handle_event(
+    event: Event,
+    http: Arc<HttpClient>,
+    bot_id: u64,
+    message_count: Arc<Mutex<HashMap<u64, u64>>>,
+) -> Result<()> {
+    if let Event::MessageCreate(msg) = event {
+        // never process bot messages
+        if msg.author.bot {
+            return Ok(());
+        }
+
+        // respond to being mentioned
+        let mentioned_self = msg
+            .mentions
+            .iter()
+            .any(|mention| mention.id == Id::new(bot_id));
+        if mentioned_self && !msg.mention_everyone {
+            let response = MESSAGES.choose(&mut rand::thread_rng()).unwrap();
+            http.create_message(msg.channel_id)
+                .reply(msg.id)
+                .content(response)?
+                .await?;
+            return Ok(());
+        }
+
+        // rare, random rude response
+        let guild_id = msg.0.guild_id.unwrap().get();
+        let mut val = 0;
+        message_count
+            .lock()
+            .unwrap()
+            .entry(guild_id)
+            .and_modify(|count| {
+                *count += 1;
+                val = *count;
+            })
+            .or_insert(0);
+        // There must have been a certain amount of messages in the server and a rare
+        // random chance for this response to trigger, after which the messages count
+        // is reset.
+        if val >= MESSAGE_RESPONSE_THRESHOLD && rand::thread_rng().gen_range(0..400) == 1 {
+            http.create_message(msg.channel_id)
+                .reply(msg.id)
+                .content("Bitch")?
+                .await?;
+            let _ = message_count.lock().unwrap().remove(&guild_id);
+        }
+    }
+    Ok(())
+}
